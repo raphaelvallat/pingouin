dca1aa9776b6cf31d1a3598f116740d3aa0e932a
Raphael <raphaelvallat9@gmail.com>
2018-06-10 09:45:07 -0700

    added test_friedman

--- pingouin/tests/test_nonparametric.py
+++ pingouin/tests/test_nonparametric.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from pingouin.tests._tests_pingouin import _TestPingouin
-from pingouin.nonparametric import (mwu, wilcoxon, kruskal)
+from pingouin.nonparametric import (mwu, wilcoxon, kruskal, friedman)
 
 np.random.seed(1234)
 x = np.random.normal(size=100)
@@ -21,6 +21,22 @@ class TestNonParametric(_TestPingouin):
         """Test function wilcoxon"""
         wilcoxon(x, y, tail='one-sided')
         wilcoxon(x, y, tail='two-sided')
+
+    def test_friedman(self):
+        """Test function friedman"""
+        df = pd.DataFrame({'DV': np.r_[x, y, z],
+                           'Time': np.repeat(['A', 'B', 'C'], 100)})
+        friedman(data=df, dv='DV', within='Time')
+        summary = friedman(data=df, dv='DV', within='Time',
+                           export_filename='test_export.csv')
+        # Compare with SciPy built-in function
+        from scipy import stats
+        Q, p = stats.friedmanchisquare(x, y, z)
+        assert np.allclose(np.round(Q, 3), summary['Q']['Friedman'])
+        assert np.allclose(p, summary['p-unc']['Friedman'])
+        # Test with NaN
+        df.loc[10, 'DV'] = np.nan
+        friedman(data=df, dv='DV', within='Time')
 
     def test_kruskal(self):
         """Test function kruskal"""
