ac36a690f7dfe6152f3ec4a13f6178e073d1af9a
Raphael Vallat <raphaelvallat9@gmail.com>
2018-07-02 15:58:45 -0700

    Added Rayleigh test and mean resultant vector in circular

--- pingouin/circular.py
+++ pingouin/circular.py
@@ -7,7 +7,7 @@ import numpy as np
 from scipy.stats import circmean
 from pingouin import _remove_na
 
-__all__ = ["circ_corrcc"]
+__all__ = ["circ_corrcc", "circ_corrcl", "circ_r", "circ_rayleigh"]
 
 
 def circ_corrcc(x, y, tail='two-sided'):
@@ -41,6 +41,7 @@ def circ_corrcc(x, y, tail='two-sided'):
     --------
     Compute the r and p-value of two circular variables
 
+        >>> from pingouin import circ_corrcc
         >>> x = [0.785, 1.570, 3.141, 3.839, 5.934]
         >>> y = [0.593, 1.291, 2.879, 3.892, 6.108]
         >>> r, pval = circ_corrcc(x, y)
@@ -71,3 +72,184 @@ def circ_corrcc(x, y, tail='two-sided'):
     pval = 2 * norm.sf(abs(tval))
     pval = pval / 2 if tail == 'one-sided' else pval
     return np.round(r, 3), pval
+
+
+def circ_corrcl(x, y, tail='two-sided'):
+    """Correlation coefficient between one circular and one linear variable
+    random variables.
+
+    Parameters
+    ----------
+    x: np.array
+        First circular variable (expressed in radians)
+    y: np.array
+        Second circular variable (linear)
+    tail: string
+        Specify whether to return 'one-sided' or 'two-sided' p-value.
+
+    Returns
+    -------
+    r: float
+        Correlation coefficient
+    pval: float
+        Uncorrected p-value
+
+    Notes
+    -----
+    Python code borrowed from brainpipe (based on the MATLAB toolbox CircStats)
+
+    Please note that NaN are automatically removed from datasets.
+
+    Examples
+    --------
+    Compute the r and p-value between one circular and one linear variables.
+
+        >>> from pingouin import circ_corrcl
+        >>> x = [0.785, 1.570, 3.141, 0.839, 5.934]
+        >>> y = [1.593, 1.291, -0.248, -2.892, 0.102]
+        >>> r, pval = circ_corrcl(x, y)
+        >>> print(r, pval)
+            0.109, 0.971
+    """
+    from scipy.stats import pearsonr, chi2
+    x = np.asarray(x)
+    y = np.asarray(y)
+
+    # Check size
+    if x.size != y.size:
+        raise ValueError('x and y must have the same length.')
+
+    # Remove NA
+    x, y = _remove_na(x, y, paired=True)
+    n = x.size
+
+    # Compute correlation coefficent for sin and cos independently
+    rxs = pearsonr(y, np.sin(x))[0]
+    rxc = pearsonr(y, np.cos(x))[0]
+    rcs = pearsonr(np.sin(x), np.cos(x))[0]
+
+    # Compute angular-linear correlation (equ. 27.47)
+    r = np.sqrt((rxc**2 + rxs**2 - 2 * rxc * rxs * rcs) / (1 - rcs**2))
+
+    # Compute p-value
+    pval = chi2.sf(n * r**2, 2)
+    pval = pval / 2 if tail == 'one-sided' else pval
+    return np.round(r, 3), pval
+
+
+def circ_r(alpha, w=None, d=None, axis=0):
+    """Mean resultant vector length for circular data.
+
+    Parameters
+    ----------
+    alpha: array
+        Sample of angles in radians
+    w: array
+        Number of incidences in case of binned angle data
+    d: float
+        Spacing (in radians) of bin centers for binned data. If supplied,
+         a correction factor is used to correct for bias in the estimation of r.
+    axis: int
+        Compute along this dimension
+
+    Returns
+    -------
+    r: float
+        Mean resultant length
+
+    Notes
+    -----
+    Python code borrowed from brainpipe (based on the MATLAB toolbox CircStats)
+
+    Examples
+    --------
+    Mean resultant vector length of circular data
+
+        >>> from pingouin import circ_r
+        >>> x = [0.785, 1.570, 3.141, 0.839, 5.934]
+        >>> circ_r(x)
+            0.497
+    """
+    alpha = np.array(alpha)
+    w = np.array(w) if w is not None else np.ones(alpha.shape)
+    if alpha.size is not w.size:
+        raise ValueError("Input dimensions do not match")
+
+    # Compute weighted sum of cos and sin of angles:
+    r = np.multiply(w, np.exp(1j * alpha)).sum(axis=axis)
+
+    # Obtain length:
+    r = np.abs(r) / w.sum(axis=axis)
+
+    # For data with known spacing, apply correction factor
+    if d is not None:
+        c = d / 2 / np.sin(d / 2)
+        r = c * r
+
+    return r
+
+
+def circ_rayleigh(alpha, w=None, d=None):
+    """Rayleigh test for non-uniformity of circular data.
+
+    Parameters
+    ----------
+    alpha: np.array
+        Sample of angles in radians.
+    w: np.array
+        Number of incidences in case of binned angle data.
+    d: float
+        Spacing (in radians) of bin centers for binned data. If supplied,
+         a correction factor is used to correct for bias in the estimation of r.
+
+    Returns
+    -------
+    z: float
+        Z-statistic
+    pval: float
+        P-value
+
+    Notes
+    -----
+    Python code borrowed from brainpipe (based on the MATLAB toolbox CircStats)
+
+    H0: the population is uniformly distributed around the circle
+    HA: the populatoin is not distributed uniformly around the circle
+
+    The assumptions for the Rayleigh test are that (1) the distribution has
+    only one mode and (2) the data is sampled from a von Mises distribution.
+
+    Examples
+    --------
+    1. Simple Rayleigh test for non-uniformity of circular data.
+
+        >>> from pingouin import circ_rayleigh
+        >>> x = [0.785, 1.570, 3.141, 0.839, 5.934]
+        >>> z, pval = circ_rayleigh(x)
+        >>> print(z, pval)
+            1.236 0.3048
+
+    2. Specifying w and d
+
+        >>> circ_rayleigh(x, w=[.1, .2, .3, .4, .5], d=0.2)
+            0.278, 0.807
+
+    """
+    alpha = np.array(alpha)
+    if w is None:
+        r = circ_r(alpha)
+        n = len(alpha)
+    else:
+        if len(alpha) is not len(w):
+            raise ValueError("Input dimensions do not match")
+        r = circ_r(alpha, w, d)
+        n = np.sum(w)
+
+    # Compute Rayleigh's statistic
+    R = n * r
+    z = (R**2) / n
+
+    # Compute p value using approxation in Zar, p. 617
+    pval = np.exp(np.sqrt(1 + 4 * n + 4 * (n**2 - R**2)) - (1 + 2 * n))
+
+    return np.round(z, 3), pval
--- pingouin/tests/test_circular.py
+++ pingouin/tests/test_circular.py
@@ -1,7 +1,7 @@
 import pytest
 import numpy as np
 from pingouin.tests._tests_pingouin import _TestPingouin
-from pingouin.circular import circ_corrcc
+from pingouin.circular import circ_corrcc, circ_corrcl, circ_r, circ_rayleigh
 
 
 class TestCircular(_TestPingouin):
@@ -20,3 +20,41 @@ class TestCircular(_TestPingouin):
         # Wrong argument
         with pytest.raises(ValueError):
             circ_corrcc(x, [0.52, 1.29, 2.87])
+
+    def test_circ_corrcl(self):
+        """Test function circ_corrcl."""
+        x = [0.785, 1.570, 3.141, 0.839, 5.934]
+        y = [1.593, 1.291, -0.248, -2.892, 0.102]
+        r, pval = circ_corrcl(x, y)
+        # Compare with the CircStats MATLAB toolbox
+        assert r == 0.109
+        assert np.round(pval, 3) == 0.971
+        _, pval2 = circ_corrcl(x, y, tail='one-sided')
+        assert pval2 == pval / 2
+        # Wrong argument
+        with pytest.raises(ValueError):
+            circ_corrcl(x, [0.52, 1.29, 2.87])
+
+    def test_circ_r(self):
+        """Test function circ_r."""
+        x = [0.785, 1.570, 3.141, 0.839, 5.934]
+        r = circ_r(x)
+        # Compare with the CircStats MATLAB toolbox
+        assert np.round(r, 3) == 0.497
+        # Wrong argument
+        with pytest.raises(ValueError):
+            circ_r(x, w=[0.1, 0.2, 0.3])
+
+    def test_circ_rayleigh(self):
+        """Test function circ_rayleigh."""
+        x = [0.785, 1.570, 3.141, 0.839, 5.934]
+        z, pval = circ_rayleigh(x)
+        # Compare with the CircStats MATLAB toolbox
+        assert z == 1.236
+        assert np.round(pval, 4) == 0.3048
+        z, pval = circ_rayleigh(x, w=[.1, .2, .3, .4, .5], d=0.2)
+        assert z == 0.278
+        assert np.round(pval, 4) == 0.8070
+        # Wrong argument
+        with pytest.raises(ValueError):
+            circ_rayleigh(x, w=[0.1, 0.2, 0.3])
