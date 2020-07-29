cbdad1f38278b95de1e07bde6f16d3b14c504044
Raphael Vallat <raphaelvallat9@gmail.com>
2020-07-28 20:02:43 -0700

    Switch to 0.3.7

--- pingouin/__init__.py
+++ pingouin/__init__.py
@@ -19,7 +19,7 @@ from .plotting import *
 from .contingency import *
 
 # Version of the package
-__version__ = "0.3.6"
+__version__ = "0.3.7"
 
 # Warn if a newer version of Pingouin is available
 from outdated import warn_if_outdated
