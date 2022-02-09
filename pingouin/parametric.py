250552ba5689c7ef693950d51a990fe73dbf0960
Ádám Nárai <narai.adam@gmail.com>
2022-02-09 19:15:30 +0100

    Update imports for scipy 1.8.0

--- pingouin/parametric.py
+++ pingouin/parametric.py
@@ -198,8 +198,12 @@ def ttest(x, y, paired=False, alternative='two-sided', correction='auto', r=.707
     array([1.971859, 0.057056])
     """
     from scipy.stats import t, ttest_rel, ttest_ind, ttest_1samp
-    from scipy.stats.stats import (_unequal_var_ttest_denom,
-                                   _equal_var_ttest_denom)
+    try:
+        from scipy.stats._stats_py import (_unequal_var_ttest_denom,
+                                       _equal_var_ttest_denom)
+    except ImportError:  # Fallback for scipy<1.8.0
+        from scipy.stats.stats import (_unequal_var_ttest_denom,
+                                       _equal_var_ttest_denom)
     from pingouin import (power_ttest, power_ttest2n, compute_effsize)
 
     # Check arguments
