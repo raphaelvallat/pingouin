Fix numerical stability issue in multivariate_normality (#292)
* Fix numerical stability issue in multivariate_normality

For large n, the lognormal variance was given by a function of log(1+x)
for x very close to zero. Numpy provides the more stable function log1p(x)
to address cases where 1+x = 1 in floating point representations, which
would otherwise incorrectly yield log(1+x) = 0.

* Fix linter error in test_multivariate.py

pingouin/multivariate.py
@@ -112,7 +112,7 @@ def multivariate_normality(X, alpha=0.05):
 
     # Lognormal mean and variance
     pmu = np.log(np.sqrt(mu**4 / (si2 + mu**2)))
-    psi = np.sqrt(np.log((si2 + mu**2) / mu**2))
+    psi = np.sqrt(np.log1p(si2 / mu**2))
 
     # P-value
     pval = lognorm.sf(hz, psi, scale=np.exp(pmu))