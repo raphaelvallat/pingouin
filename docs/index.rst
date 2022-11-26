1f329ef33d3d5513b5dbb26c8251f2bc2ac2bc5e
remrama <mallett.remy@gmail.com>
2022-11-26 11:34:32 -0600

    Plotting seaborn.FacetGrid compatibility (#314)
    * remove all fig and dpi arguments, instead generate default axis if no ax passed
    
    * set equal aspect manually in docstring demos for qqplot and circmean
    
    * rm qqplot title
    
    * move scatter_kws keyword to kwargs and color LOA span to match scatter
    
    * add qqplot kwargs for scatter
    
    * remove all fig and dpi arguments, instead generate default axis if no ax passed
    
    * set equal aspect manually in docstring demos for qqplot and circmean
    
    * rm qqplot title
    
    * move scatter_kws keyword to kwargs and color LOA span to match scatter
    
    * add qqplot kwargs for scatter
    
    * Added square keyword argument to qqplot and plot_circmean
    
    * updated changelog
    
    * Updated plotting tests
    
    * update docstring for lint passing
    
    * update kwargs using .update instead of | for prior Python compatibility
    
    * Conform kwargs updating to match other pinguoin functions
    
    * add scatter kwargs to unittests
    
    * Minor documentation update
    
    :py:method: not working + add ptests to API
    
    Co-authored-by: Raphael Vallat <raphaelvallat9@gmail.com>

--- docs/index.rst
+++ docs/index.rst
@@ -470,7 +470,7 @@ Plot the curve of achieved power given the effect size (Cohen d) and the sample 
     import pingouin as pg
     import numpy as np
     df = pg.read_dataset('mixed_anova').query("Group == 'Meditation' and Time != 'January'")
-    ax = pg.plot_paired(data=df, dv='Scores', within='Time', subject='Subject', dpi=150)
+    ax = pg.plot_paired(data=df, dv='Scores', within='Time', subject='Subject')
     ax.set_title("Effect of meditation on school performance")
 
 Integration with Pandas
--- pingouin/tests/test_plotting.py
+++ pingouin/tests/test_plotting.py
@@ -34,7 +34,7 @@ class TestPlotting(TestCase):
         assert isinstance(ax, matplotlib.axes.Axes)
         _, (ax1, ax2) = plt.subplots(1, 2, figsize=(9, 4))
         plot_blandaltman(x, y, agreement=2, confidence=None, ax=ax1)
-        plot_blandaltman(x, y, agreement=2, confidence=0.68, dpi=200, ax=ax2)
+        plot_blandaltman(x, y, agreement=2, confidence=0.68, ax=ax2)
         plt.close("all")
         # With Pingouin's dataset
         df_ba = read_dataset("blandaltman")
@@ -42,7 +42,7 @@ class TestPlotting(TestCase):
         plot_blandaltman(x, y)
         plot_blandaltman(x, y, annotate=False)
         plot_blandaltman(x, y, xaxis="x", confidence=None)
-        plot_blandaltman(x, y, xaxis="y")
+        plot_blandaltman(x, y, xaxis="y", color="green", s=10)
         plt.close("all")
 
     def test_ppoints(self):
@@ -78,7 +78,7 @@ class TestPlotting(TestCase):
         ax = qqplot(x, dist="norm")
         assert isinstance(ax, matplotlib.axes.Axes)
         _, (ax1, ax2) = plt.subplots(1, 2, figsize=(9, 4))
-        qqplot(x_exp, dist="expon", ax=ax2)
+        qqplot(x_exp, dist="expon", ax=ax2, color="black", marker="+")
         mean, std = 0, 0.8
         qqplot(x, dist=stats.norm, sparams=(mean, std), confidence=False)
         # For lognormal distribution, the shape parameter must be specified
@@ -164,6 +164,6 @@ class TestPlotting(TestCase):
         angles = np.array([0.02, 0.07, -0.12, 0.14, 1.2, -1.3])
         ax = plot_circmean(angles)
         assert isinstance(ax, matplotlib.axes.Axes)
-        ax = plot_circmean(angles, figsize=(5, 5), dpi=100, kwargs_markers={}, kwargs_arrow={})
+        ax = plot_circmean(angles, kwargs_markers={}, kwargs_arrow={})
         assert isinstance(ax, matplotlib.axes.Axes)
         plt.close("all")
