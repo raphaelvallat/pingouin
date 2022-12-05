4ca18001f4d74877f8ad02aebf367adae8c9b040
Turkalp MD <85236337+turkalpmd@users.noreply.github.com>
2022-12-06 00:28:43 +0300

    prepare for pr

--- docs/build/html/generated/pingouin-plot_shift-2.py
+++ docs/build/html/generated/pingouin-plot_shift-2.py
@@ -1,7 +1,15 @@
 import numpy as np
 import pingouin as pg
-np.random.seed(42)
-x = np.random.normal(5.5, 2, 30)
-y = np.random.normal(6, 1.5, 30)
+data = pg.read_dataset('pairwise_corr')
+
+fig = pg.plot_shift(data.Neuroticism, data.Conscientiousness,paired=True, n_boot=2000, 
+                    percentiles=[25, 50, 75],show_median=False, seed=456, violin=False)
+fig.axes[0].set_xlabel("Groups")
+fig.axes[0].set_ylabel("Values",size=15)
+fig.axes[0].set_title("Comparing of Neuroticism and  Conscientiousness",size=15)
+fig.axes[1].set_xlabel("Neuroticism quantiles",size=12)
+
+
+
 fig = pg.plot_shift(x, y, paired=True, n_boot=2000, percentiles=[25, 50, 75],
                     show_median=False, seed=456, violin=False)
