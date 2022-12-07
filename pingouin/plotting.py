653bc8b982bac54998e54967c86303e9143cdca3
Turkalp MD <85236337+turkalpmd@users.noreply.github.com>
2022-12-07 09:10:19 +0300

    first of all

--- pingouin/plotting.py
+++ pingouin/plotting.py
@@ -728,17 +728,19 @@ def plot_shift(
         >>> y = np.random.normal(6, 1.5, 50)
         >>> fig = pg.plot_shift(x, y)
 
-    With different options
+    With different options with Matplotlib directions
 
     .. plot::
 
         >>> import numpy as np
         >>> import pingouin as pg
-        >>> np.random.seed(42)
-        >>> x = np.random.normal(5.5, 2, 30)
-        >>> y = np.random.normal(6, 1.5, 30)
-        >>> fig = pg.plot_shift(x, y, paired=True, n_boot=2000, percentiles=[25, 50, 75],
-        ...                     show_median=False, seed=456, violin=False)
+        >>> data = pg.read_dataset('pairwise_corr')
+        >>> fig = pg.plot_shift(data.Neuroticism, data.Conscientiousness,paired=True, n_boot=2000,
+        ... percentiles=[25, 50, 75],show_median=False, seed=456, violin=False)
+        >>> fig.axes[0].set_xlabel("Groups")
+        >>> fig.axes[0].set_ylabel("Values",size=15)
+        >>> fig.axes[0].set_title("Comparing of Neuroticism and  Conscientiousness",size=15)
+        >>> fig.axes[1].set_xlabel("Neuroticism quantiles",size=12)
     """
     from pingouin.regression import _bias_corrected_ci
     from pingouin.nonparametric import harrelldavis as hd
