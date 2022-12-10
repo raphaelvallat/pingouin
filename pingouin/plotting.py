90c326c1ee9d4c2d4a8447dd0b509c3a66fe9814
Raphael Vallat <raphaelvallat9@gmail.com>
2022-12-10 11:19:47 -0800

    Minor edit to PR

--- pingouin/plotting.py
+++ pingouin/plotting.py
@@ -728,19 +728,20 @@ def plot_shift(
         >>> y = np.random.normal(6, 1.5, 50)
         >>> fig = pg.plot_shift(x, y)
 
-    With different options with Matplotlib directions
+    With different options, and custom axes labels
 
     .. plot::
 
         >>> import numpy as np
         >>> import pingouin as pg
-        >>> data = pg.read_dataset('pairwise_corr')
-        >>> fig = pg.plot_shift(data.Neuroticism, data.Conscientiousness,paired=True, n_boot=2000,
-        ... percentiles=[25, 50, 75],show_median=False, seed=456, violin=False)
+        >>> data = pg.read_dataset("pairwise_corr")
+        >>> fig = pg.plot_shift(data["Neuroticism"], data["Conscientiousness"], paired=True,
+        ...                     n_boot=2000, percentiles=[25, 50, 75], show_median=False, seed=456,
+        ...                     violin=False)
         >>> fig.axes[0].set_xlabel("Groups")
-        >>> fig.axes[0].set_ylabel("Values",size=15)
-        >>> fig.axes[0].set_title("Comparing of Neuroticism and  Conscientiousness",size=15)
-        >>> fig.axes[1].set_xlabel("Neuroticism quantiles",size=12)
+        >>> fig.axes[0].set_ylabel("Values", size=15)
+        >>> fig.axes[0].set_title("Comparing of Neuroticism and Conscientiousness", size=15)
+        >>> fig.axes[1].set_xlabel("Neuroticism quantiles", size=12);
     """
     from pingouin.regression import _bias_corrected_ci
     from pingouin.nonparametric import harrelldavis as hd
