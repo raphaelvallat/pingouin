023f94d2935e1122e0b4a32a2f49378d264b905a
Raphael Vallat <raphaelvallat9@gmail.com>
2022-12-10 16:15:06 -0800

    Fix doc

--- pingouin/plotting.py
+++ pingouin/plotting.py
@@ -734,14 +734,16 @@ def plot_shift(
 
         >>> import numpy as np
         >>> import pingouin as pg
+        >>> import matplotlib.pyplot as plt
         >>> data = pg.read_dataset("pairwise_corr")
         >>> fig = pg.plot_shift(data["Neuroticism"], data["Conscientiousness"], paired=True,
         ...                     n_boot=2000, percentiles=[25, 50, 75], show_median=False, seed=456,
         ...                     violin=False)
         >>> fig.axes[0].set_xlabel("Groups")
         >>> fig.axes[0].set_ylabel("Values", size=15)
-        >>> fig.axes[0].set_title("Comparing of Neuroticism and Conscientiousness", size=15)
-        >>> fig.axes[1].set_xlabel("Neuroticism quantiles", size=12);
+        >>> fig.axes[0].set_title("Comparing Neuroticism and Conscientiousness", size=15)
+        >>> fig.axes[1].set_xlabel("Neuroticism quantiles", size=12)
+        >>> plt.tight_layout()
     """
     from pingouin.regression import _bias_corrected_ci
     from pingouin.nonparametric import harrelldavis as hd
