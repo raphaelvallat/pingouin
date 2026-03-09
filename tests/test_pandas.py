94ebece66b5449ea81a203ac26c2a392ae4d9928
Raphael Vallat <raphaelvallat9@gmail.com>
2026-03-09 20:21:36 +0100

    Minor improvements

--- tests/test_pandas.py
+++ tests/test_pandas.py
@@ -99,6 +99,15 @@ class TestParametric(TestCase):
         corrs = data[["X", "Y", "M"]].pcorr()
         corrs2 = data.partial_corr(x="X", y="Y", covar="M")
         assert np.isclose(corrs.at["X", "Y"], corrs2.at["pearson", "r"])
+        # All off-diagonal values must be within [-1, 1] (clipping, improvement #7)
+        assert (corrs.values >= -1).all() and (corrs.values <= 1).all()
+        # Rank-deficient covariance matrix raises UserWarning (improvement #3)
+        import pytest
+
+        data_rank = data[["X", "Y", "M"]].copy()
+        data_rank["M2"] = data_rank["M"] + data_rank["X"]  # Perfect linear combination
+        with pytest.warns(UserWarning, match="rank-deficient"):
+            data_rank.pcorr()
 
         # Test rcorr (correlation matrix with p-values)
         # We compare against Pingouin pairwise_corr function
