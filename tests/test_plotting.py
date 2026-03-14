c2a80ba6559fc6910c04b6c3e840c9e4b2b4ca71
Raphael Vallat <raphaelvallat9@gmail.com>
2026-03-14 13:52:10 +0100

    Remove plot_shift function (#502)

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -14,7 +14,6 @@ from pingouin.plotting import (
     plot_circmean,
     plot_paired,
     plot_rm_corr,
-    plot_shift,
     qqplot,
 )
 
@@ -138,19 +137,8 @@ class TestPlotting(TestCase):
         )
         plt.close("all")
 
-    def test_plot_shift(self):
-        """Test plot_shift()."""
-        x = np.random.normal(5.5, 2, 50)
-        y = np.random.normal(6, 1.5, 50)
-        plot_shift(x, y)
-        plot_shift(
-            x, y, n_boot=100, percentiles=[5, 55, 95], show_median=False, seed=456, violin=False
-        )
-        plot_shift(x, y, paired=True, n_boot=100, percentiles=[25, 75], confidence=0.90)
-        plt.close("all")
-
     def test_plot_rm_corr(self):
-        """Test plot_shift()."""
+        """Test plot_rm_corr()."""
         df = read_dataset("rm_corr")
         g = plot_rm_corr(data=df, x="pH", y="PacO2", subject="Subject")
         g = plot_rm_corr(data=df, x="pH", y="PacO2", subject="Subject", legend=False)
